497cd1b4e793b47157bcdd395d8cf731828be787
njiutheguru <stephennjiu@gmail.com>
2025-04-29 20:23:44 +0300

    meme ui

--- memegenerator/src/components/Header.jsx
+++ memegenerator/src/components/Header.jsx
@@ -3,7 +3,7 @@ import trollFace from "../assets/troll-face.png"
 export default function Header(){
     return (
         <header className="header">
-            <img src={trollFace} alt="" />
+            <img src={trollFace}/>
             <h1>Meme Generator</h1>
         </header>
     )
